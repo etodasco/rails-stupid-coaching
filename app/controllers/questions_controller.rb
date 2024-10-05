c95a42db15f5c1cbc385de4176425a645b2810b9
Eric Todasco <erictodasco@me.com>
2024-10-05 17:02:10 -0400

    Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
     @answers = params[:answer]
     if @answers == 'I am going to work'
       @response = 'great!'
-    elsif @answers.last == "?"
+    elsif @answers.last == '?'
       @response = 'Silly questions, get dressed and go to work!.'
     else
       @response = "I don't care, get dressed and go to work!"
